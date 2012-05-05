4ae2b042ecbe65ca6e3f486c52d4b9a93731b760
Steven An <stevenan@gmail.com>
2012-05-05 17:21:25 -0400

    scroll uv

--- ScrollUV.js
+++ ScrollUV.js
@@ -2,6 +2,12 @@
 var changePerSec = Vector2( 1.0, 1.0 );
 private var offset = Vector2( 0, 0 );
 
+function Awake()
+{
+	// TEMP TEMP HACKY - we're only storing the offset for the first one..
+	offset = renderer.materials[0].GetTextureOffset( '_MainTex' );
+}
+
 function Update () {
 	offset += Time.deltaTime * changePerSec;
 
